aa5267b6dc9b1db811924c91ba3592b59881529f
Ashvin Panicker <agnjkafgh@gmail.com>
2025-04-09 21:50:08 +0530

    crop logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
-            <img src={logo} alt="Heboga Logo" className="h-24 w-24 mr-2" />
+            <img src={logo} alt="Heboga Logo" className="h-12 mr-2" />
           </div>
           
           <div className="hidden md:flex items-center space-x-8">
